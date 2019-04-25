f42ec702adc0605555c5898c3ff8ba28dafecbd3
Arkhan6 <arkhangel15@hotmail.com>
2019-04-25 12:47:38 +0200

    Cambio password mongoDB

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 // if (process.env.NODE_ENV === 'dev') {
 //     urlDB = 'mongodb: //localhost:27017/cafe';
 // } else {
-urlDB = 'mongodb+srv://udemy-nodejs:CuhENqUskddcuDS9@cluster0-brep8.mongodb.net/cafe';
+urlDB = 'mongodb+srv://udemy-nodejs:4DlHBnaRpKisV1ke@cluster0-brep8.mongodb.net/cafe';
 // }
 
 process.env.URLDB = urlDB;
